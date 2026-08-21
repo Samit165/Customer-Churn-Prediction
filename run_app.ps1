1a596aee33728067c845a9520319864598402059
Samit Kundu <kundusamit5421@gmail.com>
2026-08-21 22:38:17 +0530

    update

--- run_app.ps1
+++
@@ -1,5 +0,0 @@
-if (Test-Path "$PSScriptRoot\churn_env\Scripts\python.exe") {
-    & "$PSScriptRoot\churn_env\Scripts\python.exe" -m streamlit run app/app.py
-} else {
-    & "C:\Users\samit\anaconda3\python.exe" -m streamlit run app/app.py
-}
